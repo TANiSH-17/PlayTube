591b478175d0b6e8c5479c5b69369dfe4a74f51a
TANiSH-17 <tanish.kumar.ug23@nsut.ac.in>
2025-06-16 14:45:44 +0530

    Done till user controllers & routes related to it

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,7 @@
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
+
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
@@ -10,27 +11,21 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
-
         //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        if (!response || !response.url) {
-            throw new Error("Failed to upload file on cloudinary");
-        }   
 
         // file has been uploaded successfull
-        // remove the locally saved temporary file
+        //console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-        
         return null;
     }
 }
-
 
 
 export {uploadOnCloudinary}
