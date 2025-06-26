50091dc3a778eaf50eadce074b1a4f570eb64f16
TANiSH-17 <tanish.kumar.ug23@nsut.ac.in>
2025-06-26 19:50:29 +0530

    reverted origin value

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors({
     // This URL must match the one your React app is running on
-    origin: 'https://play-tube-hwgq.vercel.app',
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 
